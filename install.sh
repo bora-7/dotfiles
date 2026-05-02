f8de010440b28082c5d2bdeff72cde0e1749a384
Bora Akyuz <bora.akyuz@warwick.ac.uk>
2026-05-02 11:07:27 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@
 DOTFILES="$HOME/.config/dotfiles"  # adjust to wherever you clone the repo
 
 # Create symlinks
-ln -sf "$DOTFILES/ghostty"     "$HOME/.config/ghostty"
-ln -sf "$DOTFILES/nvim"        "$HOME/.config/nvim"
-ln -sf "$DOTFILES/tmux/tmux.conf" "$HOME/.tmux.conf"
-ln -sf "$DOTFILES/git/ignore"  "$HOME/.config/git/ignore"
-ln -sf "$DOTFILES/zsh/.zshrc"  "$HOME/.zshrc"
-ln -sf "$DOTFILES/zsh/.p10k.zsh" "$HOME/.p10k.zsh"
-ln -sf "$DOTFILES/gh/config.yml"  "$HOME/.config/gh/config.yml"
+ln -sf "$DOTFILES/ghostty"            "$HOME/.config/ghostty"
+ln -sf "$DOTFILES/nvim"               "$HOME/.config/nvim"
+ln -sf "$DOTFILES/tmux/tmux.conf"     "$HOME/.tmux.conf"
+ln -sf "$DOTFILES/git/ignore"         "$HOME/.config/git/ignore"
+ln -sf "$DOTFILES/git/.gitconfig"     "$HOME/.gitconfig"
+ln -sf "$DOTFILES/zsh/.zshrc"         "$HOME/.zshrc"
+ln -sf "$DOTFILES/zsh/.p10k.zsh"      "$HOME/.p10k.zsh"
+ln -sf "$DOTFILES/gh/config.yml"      "$HOME/.config/gh/config.yml"
+ln -sf "$DOTFILES/Brewfile"           "$HOME/Brewfile"
 
 echo "Dotfiles linked!"
