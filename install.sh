f22cceac976b7ed4fbcffc861800d72a3ed2fb76
Bora Akyuz <bora.akyuz@warwick.ac.uk>
2026-05-02 10:40:51 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DOTFILES="$HOME/.config/dotfiles"  # adjust to wherever you clone the repo
+
+# Create symlinks
+ln -sf "$DOTFILES/ghostty"     "$HOME/.config/ghostty"
+ln -sf "$DOTFILES/nvim"        "$HOME/.config/nvim"
+ln -sf "$DOTFILES/tmux/tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$DOTFILES/git/ignore"  "$HOME/.config/git/ignore"
+ln -sf "$DOTFILES/zsh/.zshrc"  "$HOME/.zshrc"
+ln -sf "$DOTFILES/zsh/.p10k.zsh" "$HOME/.p10k.zsh"
+ln -sf "$DOTFILES/gh/config.yml"  "$HOME/.config/gh/config.yml"
+
+echo "Dotfiles linked!"
